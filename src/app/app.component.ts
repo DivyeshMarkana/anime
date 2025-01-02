1cd9b55d77bc59e52650681f0c25fdbd41763947
Divyesh Markana <divyesh.m@ciright.com>
2025-01-02 10:26:11 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,109 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { PlyrComponent } from '@atom-platform/ngx-plyr';
+import { SwiperOptions } from 'swiper/types';
+import { SwiperContainer } from 'swiper/element';
+import { register } from 'swiper/element/bundle';
+import { Firestore, collection, getDocs, query, where } from '@angular/fire/firestore';
+
+// import SwiperCore from 'swiper';
+// import Navigation from 'swiper/modules/navigation';
+// import Pagination from 'swiper/modules/pagination';
+// import Scrollbar from 'swiper/modules/scrollbar';
+// import A11y from 'swiper/modules/a11y';
+register();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'video';
+  loading = true;
+
+  // get the component instance to have access to plyr instance
+  @ViewChild('plyrElement', { static: true }) plyr: PlyrComponent;
+  @ViewChild('swiper', { static: false }) swiper!: ElementRef<SwiperContainer>;
+
+  // or get it from plyrInit event
+  player: Plyr[] = [];
+
+
+  // http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8
+  videoSources= [
+    {
+      src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+    },
+  ];
+
+  options: SwiperOptions = {
+    speed: 500,
+    slidesPerView: 3,
+    slidesPerGroup: 1,
+    // loop: true,
+    spaceBetween: 10,
+
+    // breakpoints: {
+    //   576: {
+    //     slidesPerView: tablet
+    //   },
+    //   1200: {
+    //     slidesPerView: this.slides,
+    //     spaceBetween: desktopSpace
+    //   }
+    // }
+  };
+
+
+  slides:any = []
+
+  constructor(private firestore: Firestore){}
+
+  
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+      this.getList();
+    }, 3000);
+  }
+
+  ngAfterViewInit(): void {
+    Object.assign(this.swiper.nativeElement, this.options);
+    this.swiper.nativeElement.initialize();
+  }
+
+  initPlyr(event:any){
+    this.player = event;
+  }
+
+  played(event: any) {
+    console.log('played', event);
+  }
+
+  play(): void {
+    // this.player.play(); // or this.plyr.player.play()
+  }
+
+  getList(){
+    const q = query(collection(this.firestore, "anime"), where("groups", "array-contains", 'Recommendation'));
+
+    (() => {
+      getDocs(q).then((snapshot) => {
+        let data: any[] = []
+        snapshot.forEach(doc => {
+          data.push({ ...doc.data(), id: doc.id })
+        });
+
+        console.log(data);
+        
+        this.slides = data;
+        // this.loadingLatest = false;
+        // console.log(this.latestList);
+        // this._change.detectChanges();
+      })
+    })();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
+import { PlyrModule } from '@atom-platform/ngx-plyr';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PlyrModule,
+    AppRoutingModule,
+  
   ],
-  providers: [],
+  providers: [
+    // provideFirebaseApp(() => initializeApp({"projectId":"anime-cloud-b07fc","appId":"1:185931984465:web:a5cc7ecbf25477a7475270","storageBucket":"anime-cloud-b07fc.appspot.com","locationId":"us-central","apiKey":"AIzaSyBK4-aCmgdGi8-dk7puQuc3ILkCmGEcjwQ","authDomain":"anime-cloud-b07fc.firebaseapp.com","messagingSenderId":"185931984465","measurementId":"G-2X5VW5NE4N"})),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
